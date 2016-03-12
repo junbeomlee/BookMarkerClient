54af38b36fb80f0113d75da8ee6b1e1a39dc1e9c
jun <leebduk@gmail.com>
2016-03-12 23:43:35 +0900

    class sift moved

---
+++ app/src/main/java/com/example/leejunbeom/bookMarker/dagger/testComponent.java
@@ -0,0 +1,14 @@
+package com.example.leejunbeom.bookMarker.dagger;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Jun on 16. 3. 12..
+ */
+@Singleton
+@Component
+public interface testComponent {
+    
+}
