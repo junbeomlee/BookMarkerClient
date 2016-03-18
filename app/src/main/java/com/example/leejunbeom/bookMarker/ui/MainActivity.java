e8616d6c0c74b77170ba5e51e1d76a958c6f5d0b
jun <leebduk@gmail.com>
2016-03-18 11:56:07 +0900

    qrcode 수정

--- app/src/main/java/com/example/leejunbeom/bookMarker/ui/MainActivity.java
+++ app/src/main/java/com/example/leejunbeom/bookMarker/ui/MainActivity.java
@@ -4,12 +4,15 @@ package com.example.leejunbeom.bookMarker.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.leejunbeom.bookMarker.dagger.injector;
 import com.example.leejunbeom.bookMarker.model.SIFT;
+import com.example.leejunbeom.test.R;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,30 +29,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //injector.get().inject(this);
-        /*IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setCaptureActivity(QRCodeActivity.class);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        //integrator.setCaptureActivity(QRCodeActivity.class);
         integrator.setOrientationLocked(true);
-        integrator.initiateScan();*/
+        integrator.initiateScan();
 
-        Intent intent = new Intent(
+        /*Intent intent = new Intent(
                 "com.google.zxing.client.android.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-        startActivityForResult(intent, 0);
+        startActivityForResult(intent, 0);*/
 
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
+        if ((scanResult != null) && (scanResult.getContents() != null)) {
+            String data = scanResult.getContents();
 
+            Toast.makeText(this, data,
+                        Toast.LENGTH_LONG).show();
 
-                String contents = intent.getStringExtra("SCAN_RESULT"); // This will contain your scan result
-                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                Toast.makeText(MainActivity.this, contents, Toast.LENGTH_SHORT).show();
-
-            }
         }
     }
     @Override
