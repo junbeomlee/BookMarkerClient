3cef12dee5e2d8cf5294103139fdabe3a8483ed9
noduritoto <noduritoto@gmail.com>
2016-03-27 16:08:06 +0900

    SwipeMenuListView Library is Added and set

--- Library/opencv/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ Library/opencv/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/leejunbeom/AndroidStudioProjects/BookMarker/Library/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\noduritoto\\Desktop\\noduritest4\\BookMarkerClient\\Library\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
