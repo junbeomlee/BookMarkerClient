9b36809446a37c44a6b560dc15f917e0d69f0d42
jun <leebduk@gmail.com>
2016-05-16 12:41:58 +0900

    done onframe

--- app/src/main/java/com/example/leejunbeom/bookMarker/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/leejunbeom/bookMarker/ui/activity/MainActivity.java
@@ -157,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements Mainscreen{
     //test
     @Override
     public void launchNaviActivity() {
-        Intent intent = new Intent(this,NaviActivity.class);
+        Intent intent = new Intent(this,SearchActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/leejunbeom/bookMarker/ui/preview/CameraPreview.java
+++ app/src/main/java/com/example/leejunbeom/bookMarker/ui/preview/CameraPreview.java
@@ -2,115 +2,133 @@ package com.example.leejunbeom.bookMarker.ui.preview;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
  * Created by Jun on 16. 4. 11..
  */
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-    String TAG = "CAMERA";
-    private SurfaceHolder mHolder;
-    private Camera mCamera;
+public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCallback {
 
-    public CameraPreview(Context context, Camera camera) {
-        super(context);
-        mCamera = camera;
+    private Camera mCamera = null;
+    private ImageView MyCameraPreview = null;
+    private Bitmap bitmap = null;
+    private int[] pixels = null;
+    private byte[] FrameData = null;
+    private int imageFormat;
+    private int PreviewSizeWidth;
+    private int PreviewSizeHeight;
+    private boolean bProcessing = false;
+    private ImageView imageView2;
 
-        // SurfaceHolder 가 가지고 있는 하위 Surface가 파괴되거나 업데이트 될경우 받을 콜백을 세팅한다
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        // deprecated 되었지만 3.0 이하 버젼에서 필수 메소드라서 호출해둠.
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    Handler mHandler = new Handler(Looper.getMainLooper());
+
+    public CameraPreview(int PreviewlayoutWidth, int PreviewlayoutHeight,
+                         ImageView CameraPreview, ImageView imageView)
+    {
+        PreviewSizeWidth = PreviewlayoutWidth;
+        PreviewSizeHeight = PreviewlayoutHeight;
+        imageView2=imageView;
+        MyCameraPreview = CameraPreview;
+        bitmap = Bitmap.createBitmap(PreviewSizeWidth, PreviewSizeHeight, Bitmap.Config.ARGB_8888);
+        pixels = new int[PreviewSizeWidth * PreviewSizeHeight];
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        // Surface가 생성되었으니 프리뷰를 어디에 띄울지 지정해준다. (holder 로 받은 SurfaceHolder에 뿌려준다.
-        try {
-            Camera.Parameters parameters = mCamera.getParameters();
-            if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-                parameters.set("orientation", "portrait");
-                mCamera.setDisplayOrientation(90);
-                parameters.setRotation(90);
-            } else {
-                parameters.set("orientation", "landscape");
-                mCamera.setDisplayOrientation(0);
-                parameters.setRotation(0);
-            }
-            mCamera.setParameters(parameters);
+    public void onPreviewFrame(byte[] arg0, Camera arg1) {
+        // At preview mode, the frame data will push to here.
+        if (imageFormat == ImageFormat.NV21) {
+            //We only accept the NV21(YUV420) format.
+            if (!bProcessing) {
+                FrameData = arg0;
+                imageView2.setImageBitmap(bitmap);
+                // mHandler.post(DoImageProcessing);
+                Camera.Parameters params = mCamera.getParameters();
 
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+
+                int w = params.getPreviewSize().width;
+
+                int h = params.getPreviewSize().height;
+
+                @SuppressWarnings("deprecation")
+                int format = params.getPreviewFormat();
+
+                YuvImage image = new YuvImage(arg0, format, w, h, null);
+
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+                Rect area = new Rect(0, 0, w, h);
+
+                image.compressToJpeg(area, 50, out);
+                System.out.println("getPreviewSize().width : "+w+"\n"
+                        +"params.getPreviewSize().height : "+h);
+
+                Bitmap asdbitmap = BitmapFactory.decodeByteArray(out.toByteArray(), 0, out.size());
+                imageView2.setImageBitmap(asdbitmap);
+
+            }
         }
     }
 
+    public void onPause()
+    {
+        mCamera.stopPreview();
+    }
+
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // 프리뷰 제거시 카메라 사용도 끝났다고 간주하여 리소스를 전부 반환한다
-        if (mCamera != null) {
-            mCamera.stopPreview();
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3)
+    {
+        Camera.Parameters parameters;
+
+        parameters = mCamera.getParameters();
+        // Set the camera preview size
+        parameters.setPreviewSize(PreviewSizeWidth, PreviewSizeHeight);
+
+        imageFormat = parameters.getPreviewFormat();
+
+        mCamera.setParameters(parameters);
+
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0)
+    {
+        mCamera = Camera.open();
+        try
+        {
+            // If did not set the SurfaceHolder, the preview area will be black.
+            mCamera.setPreviewDisplay(arg0);
+            mCamera.setPreviewCallback(this);
+        }
+        catch (IOException e)
+        {
             mCamera.release();
             mCamera = null;
         }
     }
 
-    private Camera.Size getBestPreviewSize(int width, int height)
-    {
-        Camera.Size result=null;
-        Camera.Parameters p = mCamera.getParameters();
-        for (Camera.Size size : p.getSupportedPreviewSizes()) {
-            if (size.width<=width && size.height<=height) {
-                if (result==null) {
-                    result=size;
-                } else {
-                    int resultArea=result.width*result.height;
-                    int newArea=size.width*size.height;
-
-                    if (newArea>resultArea) {
-                        result=size;
-                    }
-                }
-            }
-        }
-        return result;
-
-    }
-
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // 프리뷰를 회전시키거나 변경시 처리를 여기서 해준다.
-        // 프리뷰 변경시에는 먼저 프리뷰를 멈춘다음 변경해야한다.
-        if (mHolder.getSurface() == null){
-            // 프리뷰가 존재하지 않을때
-            return;
-        }
-
-        // 우선 멈춘다
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e){
-            // 프리뷰가 존재조차 하지 않는 경우다
-        }
-
-        // 프리뷰 변경, 처리 등을 여기서 해준다.
-        Camera.Parameters parameters = mCamera.getParameters();
-        Camera.Size size = getBestPreviewSize(w, h);
-        parameters.setPreviewSize(size.width, size.height);
-        mCamera.setParameters(parameters);
-        // 새로 변경된 설정으로 프리뷰를 재생성한다
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-
-        } catch (Exception e){
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
-        }
+    public void surfaceDestroyed(SurfaceHolder arg0)
+    {
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
     }
+
 }
