a5c8fc703907b0ca6a3d76fa3320ee335a6c84d1
Younghoon Kim <noduritoto@gmail.com>
2016-05-31 11:55:08 +0900

    ocr 160531

---
+++ app/src/main/java/com/example/leejunbeom/bookMarker/model/OCR.java
@@ -0,0 +1,7 @@
+package com.example.leejunbeom.bookMarker.model;
+
+/**
+ * Created by noduritoto on 2016. 5. 30..
+ */
+public class OCR {
+}
--- app/src/main/java/com/example/leejunbeom/bookMarker/ui/activity/BookAddOCRActivity.java
+++ app/src/main/java/com/example/leejunbeom/bookMarker/ui/activity/BookAddOCRActivity.java
@@ -2,10 +2,15 @@ package com.example.leejunbeom.bookMarker.ui.activity;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.hardware.Camera;
+import android.media.ExifInterface;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,11 +25,14 @@ import android.widget.Toast;
 
 import com.example.leejunbeom.bookMarker.ui.preview.CameraPreview;
 import com.example.leejunbeom.test.R;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -34,6 +42,18 @@ public class BookAddOCRActivity extends AppCompatActivity {
     private Context mContext = this;
     private Camera mCamera;
     private CameraPreview mPreview;
+
+    //for OCR
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
+
+    // You should have the trained data file in assets folder
+    // You can get them at:
+    // http://code.google.com/p/tesseract-ocr/downloads/list
+    public static final String lang = "eng";
+    private static final String TAG1 = "SimpleAndroidOCR.java";
+    protected String _path;
+    private String resultString;
 
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
         @Override
@@ -52,12 +72,107 @@ public class BookAddOCRActivity extends AppCompatActivity {
                 fos.write(data);
                 fos.close();
                 //Thread.sleep(500);
+
+                // ocr 프로세스 시작
+                _path = pictureFile.getAbsolutePath();
+                String TAG1 = "noduriOCR";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+
+                //_path = DATA_PATH + "ocr2.jpg";
+                Log.i("photo path : ", _path);
+
+                Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+
+                // bitmap image modified needed
+
+                try {
+                    ExifInterface exif = new ExifInterface(_path);
+                    int exifOrientation = exif.getAttributeInt(
+                            ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL);
+
+                    Log.v(TAG1, "Orient: " + exifOrientation);
+
+                    int rotate = 0;
+
+                    switch (exifOrientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotate = 90;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotate = 180;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotate = 270;
+                            break;
+                    }
+
+                    Log.v(TAG1, "Rotation: " + rotate);
+
+                    if (rotate != 0) {
+
+                        // Getting width & height of the given image.
+                        int w = bitmap.getWidth();
+                        int h = bitmap.getHeight();
+
+                        // Setting pre rotate
+                        Matrix mtx = new Matrix();
+                        mtx.preRotate(rotate);
+
+                        // Rotating Bitmap
+                        bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+                    }
+
+                    // Convert to ARGB_8888, required by tess
+                    bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+                } catch (IOException e) {
+                    Log.e(TAG1, "Couldn't correct orientation: " + e.toString());
+                }
+
+                // _image.setImageBitmap( bitmap );
+
+                Log.v(TAG1, "Before baseApi");
+
+                TessBaseAPI baseApi = new TessBaseAPI();
+                baseApi.setDebug(true);
+                baseApi.init(DATA_PATH, lang);
+                baseApi.setImage(bitmap);
+
+                String recognizedText = baseApi.getUTF8Text();
+
+                baseApi.end();
+
+                // You now have the text in recognizedText var, you can do anything with it.
+                // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+                // so that garbage doesn't make it to the display.
+
+                Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+                if ( lang.equalsIgnoreCase("eng") ) {
+                    recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+                }
+
+                recognizedText = recognizedText.trim();
+                Log.v(TAG, "RESULT TEXT: " + recognizedText);
+                /*
+
+                if ( recognizedText.length() != 0 ) {
+                    _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+                    _field.setSelection(_field.getText().toString().length());
+                }
+                */
+
+                // Cycle done.
+
+
                 mCamera.startPreview();
 
             } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
+                Log.d(TAG1, "File not found: " + e.getMessage());
             } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
+                Log.d(TAG1, "Error accessing file: " + e.getMessage());
             } /*catch (InterruptedException e) {
 				e.printStackTrace();
 			}*/
@@ -68,6 +183,55 @@ public class BookAddOCRActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //OCR DATA PATH ////////////////////////////////////////////////////////
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+
+        _path = DATA_PATH + "/ocr.jpg";
+        ////////////////////////////////////////////////////////////////////////
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -149,7 +313,9 @@ public class BookAddOCRActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
 
+        //mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
         mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+        //this._path = new String(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
         Log.i("MyCamera", "Saved at"+Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
 
         return mediaFile;
@@ -177,12 +343,6 @@ class ScanLineView extends View {
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.CYAN);                    // Cyan
         paint.setStrokeWidth(10);                     // 크기 10
-
-        Paint paint2 = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.RED);                    // Cyan
-        paint.setStrokeWidth(10);                     // 크기 10
-
 
         int centerX = getWidth()/2;
         int centerY = getHeight()/2;
